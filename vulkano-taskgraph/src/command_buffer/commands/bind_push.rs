fbc5aa117463ca4da24f4a18272e62198a25f45d
marc0246 <40955683+marc0246@users.noreply.github.com>
2024-10-18 11:40:13 +0200

    Task graph [7/10]: descriptor sets

--- vulkano-taskgraph/src/command_buffer/commands/bind_push.rs
+++ vulkano-taskgraph/src/command_buffer/commands/bind_push.rs
@@ -1,5 +1,6 @@
 use crate::{
     command_buffer::{RecordingCommandBuffer, Result},
+    descriptor_set::GLOBAL_SET,
     Id,
 };
 use ash::vk;
@@ -10,7 +11,8 @@ use vulkano::{
     buffer::{Buffer, BufferContents, IndexType},
     device::DeviceOwned,
     pipeline::{
-        ray_tracing::RayTracingPipeline, ComputePipeline, GraphicsPipeline, PipelineLayout,
+        ray_tracing::RayTracingPipeline, ComputePipeline, GraphicsPipeline, Pipeline,
+        PipelineBindPoint, PipelineLayout,
     },
     DeviceSize, Version, VulkanObject,
 };
@@ -86,6 +88,14 @@ impl RecordingCommandBuffer<'_> {
             )
         };
 
+        let invalidate_from = self
+            .state
+            .invalidate_descriptor_sets(PipelineBindPoint::Compute, pipeline.layout());
+
+        if invalidate_from == Some(GLOBAL_SET) {
+            self.bind_global_set(PipelineBindPoint::Compute, pipeline.layout());
+        }
+
         self.death_row.push(pipeline.clone());
 
         self
@@ -111,6 +121,14 @@ impl RecordingCommandBuffer<'_> {
                 pipeline.handle(),
             )
         };
+
+        let invalidate_from = self
+            .state
+            .invalidate_descriptor_sets(PipelineBindPoint::Graphics, pipeline.layout());
+
+        if invalidate_from == Some(GLOBAL_SET) {
+            self.bind_global_set(PipelineBindPoint::Graphics, pipeline.layout());
+        }
 
         self.death_row.push(pipeline.clone());
 
